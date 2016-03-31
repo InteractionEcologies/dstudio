d816a913548f905168eafe6269fb3244c5cd21d0
Shriti-UCI <shriti.raj13@gmail.com>
2016-03-31 15:41:08 -0400

    changes in Utils for notification times and randomizations

--- app/src/main/java/edu/umich/dstudio/utils/Utils.java
+++ app/src/main/java/edu/umich/dstudio/utils/Utils.java
@@ -1,11 +1,63 @@
 package edu.umich.dstudio.utils;
 
+
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 /**
  * Created by neera_000 on 3/26/2016.
  */
 public class Utils {
+
+    public static HashMap<Integer, HashMap<String, Integer>> intervals = new HashMap<Integer, HashMap<String, Integer>>();
+
+    public static void setIntervalsForMoodRandomization(int n){
+        int numberOfPromptsNeeded = n;
+        GSharedPreferences gSharedPreferences = GSharedPreferences.getInstance();
+        String wakeUpTime = gSharedPreferences.getPreference("startTime");
+        String bedTime = gSharedPreferences.getPreference("endTime");
+
+        int startTime = convertStringTimetoSeconds(wakeUpTime);
+        int endTime = convertStringTimetoSeconds(bedTime);
+        int intervalLenghtInSeconds = (endTime - startTime)/numberOfPromptsNeeded;
+
+        int start;
+        int end = startTime;
+
+        for(int i=0; i<n; i++){
+            HashMap<String, Integer> map = new HashMap<>();
+            start = end;
+            map.put("start", start);
+            end = end + intervalLenghtInSeconds;
+            map.put("end", end);
+            intervals.put(i, map);
+        }
+
+    }
+
+    public static int[] getTimesForRandomMoodPrompts() {
+        //get intervals
+        //generate random numbers between those intervals
+        LinkedList<Integer> notificationTimes = new LinkedList<>();
+        Random rand = new Random();
+        Iterator<Map.Entry<Integer, HashMap<String, Integer>>> iterator = intervals.entrySet().iterator();
+        while(iterator.hasNext()){
+            Map.Entry<Integer, HashMap<String, Integer>> entry = iterator.next();
+            HashMap<String, Integer> value = entry.getValue();
+            int max = value.get("end");
+            int min = value.get("start");
+            int diff =  max-min;
+            int randomNum = rand.nextInt(diff + 1) + min;
+            notificationTimes.add(randomNum);
+
+        }
+        return toIntArray(notificationTimes);
+    }
 
     /**
      * Given an email Id as string, encodes it so that it can go into the firebase object without
@@ -34,7 +86,6 @@ public class Utils {
         };
     }
 
-
     /**
      * Gets all the times at which a notification needs to be shown, and check if the current time
      * is within a window of 15 minutes from that time. Returns true if that is the case, false
@@ -49,15 +100,39 @@ public class Utils {
         c.set(Calendar.MINUTE, 0);
         c.set(Calendar.SECOND, 0);
         c.set(Calendar.MILLISECOND, 0);
-        long passed = now - c.getTimeInMillis();
-        long secondsPassed = passed / 1000;
+        int midnightSeconds = (int) c.getTimeInMillis()/1000;
+        int secondsPassedAfterMidnight = (int) now/1000 - midnightSeconds;
+
+        GSharedPreferences gSharedPreferences = GSharedPreferences.getInstance();
+        String wakeUpTime = gSharedPreferences.getPreference("startTime");
+        String bedTime = gSharedPreferences.getPreference("endTime");
+
+        int secondsPassedAfterMidnightAtStartTime = convertStringTimetoSeconds(wakeUpTime) - midnightSeconds;
+        int secondsPassedAfterMidnightAtEndTime = convertStringTimetoSeconds(bedTime) - midnightSeconds;
 
         for(int i:getTimesForNotification()) {
-            if(secondsPassed - i > 0 && secondsPassed - i < 899) {
+            if(secondsPassedAfterMidnight> secondsPassedAfterMidnightAtStartTime && secondsPassedAfterMidnight < secondsPassedAfterMidnightAtEndTime) {
                 return true;
             }
         }
         return false;
+    }
+
+    public static int convertStringTimetoSeconds(String time){
+        int secondsPassed = 0;
+        String [] timeString = time.split(":");
+        int hour = Integer.parseInt(timeString[0]);
+        int minutes = Integer.parseInt(timeString[1]);
+        secondsPassed = (hour*60 + minutes)*60;
+        return secondsPassed;
+    }
+
+    public static int[] toIntArray(List<Integer> integerList) {
+        int[] intArray = new int[integerList.size()];
+        for (int i = 0; i < integerList.size(); i++) {
+            intArray[i] = integerList.get(i);
+        }
+        return intArray;
     }
 
 
